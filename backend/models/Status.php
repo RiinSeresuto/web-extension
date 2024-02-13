c65fd938288f917763821b9a26c87ddd449e1013
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-13 11:44:18 +0800

    added status model

---
+++ backend/models/Status.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cms_status".
+ *
+ * @property int $id
+ * @property string $status_type
+ *
+ * @property CmsCategory[] $cmsCategories
+ * @property CmsConnectedAgencies[] $cmsConnectedAgencies
+ * @property CmsForm[] $cmsForms
+ * @property CmsMenu[] $cmsMenus
+ * @property CmsPages[] $cmsPages
+ */
+class Status extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cms_status';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'status_type'], 'required'],
+            [['id'], 'integer'],
+            [['status_type'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'status_type' => 'Status Type',
+        ];
+    }
+
+    /**
+     * Gets query for [[CmsCategories]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsCategories()
+    {
+        return $this->hasMany(CmsCategory::className(), ['status_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CmsConnectedAgencies]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsConnectedAgencies()
+    {
+        return $this->hasMany(CmsConnectedAgencies::className(), ['status_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CmsForms]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsForms()
+    {
+        return $this->hasMany(CmsForm::className(), ['status_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CmsMenus]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsMenus()
+    {
+        return $this->hasMany(CmsMenu::className(), ['status_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CmsPages]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsPages()
+    {
+        return $this->hasMany(CmsPages::className(), ['status_id' => 'id']);
+    }
+}
