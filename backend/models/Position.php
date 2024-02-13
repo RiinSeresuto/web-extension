1eefd3511189e6c60b49d84ff57cd1ec141b50e1
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-13 11:50:04 +0800

    added position model

---
+++ backend/models/Position.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cms_position".
+ *
+ * @property int $id
+ * @property string $position
+ *
+ * @property CmsMenu[] $cmsMenus
+ */
+class Position extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cms_position';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'position'], 'required'],
+            [['id'], 'integer'],
+            [['position'], 'string', 'max' => 255],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'position' => 'Position',
+        ];
+    }
+
+    /**
+     * Gets query for [[CmsMenus]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsMenus()
+    {
+        return $this->hasMany(CmsMenu::className(), ['position_id' => 'id']);
+    }
+}
