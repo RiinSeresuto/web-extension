bdd68912a111b45a8b330a9d94ef81cc26780fa8
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-13 16:43:18 +0800

    added search model for menu

---
+++ backend/models/MenuSearch.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace backend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Menu;
+
+/**
+ * MenuSearch represents the model behind the search form of `app\models\Menu`.
+ */
+class MenuSearch extends Menu
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'parent_id', 'menu_order', 'position_id', 'status_id', 'logo_file', 'user_id', 'user_update_id'], 'integer'],
+            [['label', 'link', 'date_created', 'date_updated'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Menu::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'parent_id' => $this->parent_id,
+            'menu_order' => $this->menu_order,
+            'position_id' => $this->position_id,
+            'status_id' => $this->status_id,
+            'logo_file' => $this->logo_file,
+            'user_id' => $this->user_id,
+            'user_update_id' => $this->user_update_id,
+            'date_created' => $this->date_created,
+            'date_updated' => $this->date_updated,
+        ]);
+
+        $query->andFilterWhere(['like', 'label', $this->label])
+            ->andFilterWhere(['like', 'link', $this->link]);
+
+        return $dataProvider;
+    }
+}
