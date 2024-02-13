a8105f1f6f199722fd091bc1140e5f0fb459b735
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-13 13:23:20 +0800

    added user history model

---
+++ backend/models/UserHistory.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_history".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string $username
+ * @property string $email
+ * @property string $auth_key
+ * @property string|null $registration_ip
+ * @property int|null $confirmed_at
+ * @property int|null $blocked_at
+ * @property int $created_at
+ * @property int $updated_at
+ */
+class UserHistory extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_history';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'confirmed_at', 'blocked_at', 'created_at', 'updated_at'], 'integer'],
+            [['username', 'email', 'auth_key', 'created_at', 'updated_at'], 'required'],
+            [['username', 'email'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['registration_ip'], 'string', 'max' => 45],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'username' => 'Username',
+            'email' => 'Email',
+            'auth_key' => 'Auth Key',
+            'registration_ip' => 'Registration Ip',
+            'confirmed_at' => 'Confirmed At',
+            'blocked_at' => 'Blocked At',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
