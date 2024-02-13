cf3fed63d1133ee988cb47578d2c60a6a4ab235b
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-13 11:27:00 +0800

    modified assets

--- backend/assets/LoginAsset.php
+++ backend/assets/LoginAsset.php
@@ -7,14 +7,13 @@ use yii\web\AssetBundle;
 /**
  * Main backend application asset bundle.
  */
-class AppAsset extends AssetBundle
+class LoginAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
         'css/bootstrap.min.css',
         'css/site.css',
-        //'css/fontawesome.min.css'
     ];
     public $js = [
     ];
