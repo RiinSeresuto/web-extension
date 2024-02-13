ace57258e4087b7025c27de6f9a765678a5ead83
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-13 16:41:22 +0800

    fixed background color

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -9,7 +9,8 @@ use yii\bootstrap\NavBar;
 
 <header class="main-header" style="background:#222d32; !important">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    
+    <?= Html::a('<span class="logo-mini">Web</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo', 'style' => 'background-color: #222d32']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation" style="background:#222d32;">
 
