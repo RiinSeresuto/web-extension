c27cef0450fcd0218e9a1e4b482b94c783249ceb
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-14 15:09:53 +0800

    added model for widget type

---
+++ backend/models/WidgetType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cms_widget_type".
+ *
+ * @property int $id
+ * @property string $widget_type
+ *
+ * @property CmsField[] $cmsFields
+ */
+class WidgetType extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cms_widget_type';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'widget_type'], 'required'],
+            [['id'], 'integer'],
+            [['widget_type'], 'string', 'max' => 255],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'widget_type' => 'Widget Type',
+        ];
+    }
+
+    /**
+     * Gets query for [[CmsFields]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsFields()
+    {
+        return $this->hasMany(CmsField::className(), ['widget_type_id' => 'id']);
+    }
+}
