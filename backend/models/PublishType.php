65b98b70b02c60a77da564e3b8f3706fd3cdf4ba
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-14 15:08:24 +0800

    added model for publish type

---
+++ backend/models/PublishType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cms_publish_type".
+ *
+ * @property int $id
+ * @property string $publish_type
+ *
+ * @property CmsPost[] $cmsPosts
+ */
+class PublishType extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cms_publish_type';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'publish_type'], 'required'],
+            [['id'], 'integer'],
+            [['publish_type'], 'string', 'max' => 255],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'publish_type' => 'Publish Type',
+        ];
+    }
+
+    /**
+     * Gets query for [[CmsPosts]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsPosts()
+    {
+        return $this->hasMany(CmsPost::className(), ['publish_id' => 'id']);
+    }
+}
