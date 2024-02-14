2239a91b32ee322f8f8e3e780d55758b764c6ec4
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-14 15:09:00 +0800

    added model for visibility type

---
+++ backend/models/VisibilityType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cms_visibility_type".
+ *
+ * @property int $id
+ * @property string $visibility_type
+ *
+ * @property CmsPost[] $cmsPosts
+ */
+class VisibilityType extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cms_visibility_type';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'visibility_type'], 'required'],
+            [['id'], 'integer'],
+            [['visibility_type'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'visibility_type' => 'Visibility Type',
+        ];
+    }
+
+    /**
+     * Gets query for [[CmsPosts]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCmsPosts()
+    {
+        return $this->hasMany(CmsPost::className(), ['visibility_id' => 'id']);
+    }
+}
