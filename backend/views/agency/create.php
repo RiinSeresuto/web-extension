1bc60c9d9559775805d92282f3f4d9002b839c18
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-14 16:02:57 +0800

    added crud for agency

---
+++ backend/views/agency/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\ConnectedAgencies */
+
+$this->title = 'Create Connected Agencies';
+$this->params['breadcrumbs'][] = ['label' => 'Connected Agencies', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="connected-agencies-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
