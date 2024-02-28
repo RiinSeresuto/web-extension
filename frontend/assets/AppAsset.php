1360eac3f725e68a5fe447fae62b598f7c668818
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-28 15:48:27 +0800

    -

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,8 +13,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'slick/slick.css'
     ];
     public $js = [
+        'slick/slick.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
