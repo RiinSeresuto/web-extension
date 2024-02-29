d50e2a086780e6f03d244bbd64eb6c2f2b19bd55
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-02-29 17:48:49 +0800

    added function for looping of parent and children menu displaying

--- frontend/views/layouts/main-menu.php
+++ frontend/views/layouts/main-menu.php
@@ -9,31 +9,36 @@ $mainMenu = [];
             $mainMenu[] = $menu;
         }
     }
+
+    function generateNavItem($menuChildren) {
+        $start_ul = "<ul class='child-nav'>";
+        $end_ul = "</ul>";
+
+        $return_item = "";
+
+        foreach($menuChildren as $children){
+            if(!empty($children->menuChildren)){
+                $return_item = $return_item . '<li class="child-item"><a href="" class="active">' . $children->label . '</a>'; //parent of children
+                $temp = generateNavItem($children->menuChildren); //children
+                $return_item = $return_item . $temp . '</li>'; 
+            } else {
+                $return_item = $return_item . '<li class="child-item"><a href="" class="active">' . $children->label . '</a></li>'; //parent w/o children
+            }
+        }
+
+        return $start_ul . $return_item . $end_ul;
+    }
 ?> 
     
     <ul class="main-menu-navs">
-        <div class="dropdown"> 
-            <button class="dropbtn">Dropdown</button> <!--Parent Menu-->
-            <div class="dropdown-content">
-                <a href="#">Link 1</a> <!--Children-->
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-            </div>
-        </div>
-
         <?php foreach ($mainMenu as $menu): ?>
-            <li><a href="" class="active"><?= $menu->label ?></a>
-                
-            </li>
-
-            <?php if ($menu->menuChildren) {
-                // echo '<pre>';
-                // print_r ($menu->menuChildren);
-                // exit;
-                    // return $menu->parentLabel->label;
-                }
-            ?>
-
-            
+            <?php if(!empty($menu->menuChildren)){ ?>
+                <li class="parent-nav">
+                    <a href="" class="active"><?= $menu->label ?></a>
+                    <?= generateNavItem($menu->menuChildren) ?>
+                </li>
+            <?php } else { ?>
+                <li><a href="" class="active"><?= $menu->label ?></a></li>
+            <?php } ?>
         <?php endforeach; ?>
     </ul>
