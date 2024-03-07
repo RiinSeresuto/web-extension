2e67f119ac16b8a7b2f770da0c7a9cb107cc4ddc
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-03-07 19:13:53 +0800

    modify function index

---
+++ frontend/controllers/ContentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace frontend\controllers;
+
+use backend\models\Pages;
+
+class ContentController extends \yii\web\Controller
+{
+    public function actionIndex($type, $menu_id)
+    {
+        if ($type == 1) {
+            $page = Pages::find()->where(['menu_id' => $menu_id])->one();
+            
+            return $this->render('index', ['page' => $page]);
+
+        }
+    }
+
+}
