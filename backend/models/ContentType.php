001b393af895ae7916e50a0cae24bac1ee677f4b
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-03-07 19:05:58 +0800

    added content type model

---
+++ backend/models/ContentType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cms_content_type".
+ *
+ * @property int $id
+ * @property string $content_label
+ */
+class ContentType extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cms_content_type';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['content_label'], 'required'],
+            [['content_label'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'content_label' => 'Content Label',
+        ];
+    }
+}
