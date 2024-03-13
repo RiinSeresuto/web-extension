f49ff668c692d7eed0c45b9bf47cff30fba78409
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-03-13 16:43:10 +0800

    fixed saving, updating and viewing

--- backend/views/connected-agencies/view.php
+++ backend/views/connected-agencies/view.php
@@ -6,19 +6,18 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model backend\models\ConnectedAgencies */
 
-$this->title = $model->id;
+$this->title = $model->label;
 $this->params['breadcrumbs'][] = ['label' => 'Connected Agencies', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="connected-agencies-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('<i class="fas fa-arrow-circle-left"></i> Back', ['index', 'id' => $model->id], ['class' => 'btn btn-secondary btn-sm']) ?>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
+            'class' => 'btn btn-danger btn-sm',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
@@ -29,16 +28,37 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
-            'agency_type_id',
+            [
+                'attribute' => 'agency_type_id',
+                'value' => function($model){
+                    return $model->agencyType->agency_type;
+                },
+            ],
             'label',
             'agency_order',
-            'status_id',
+            [
+                'attribute' => 'status_id',
+                'value' => function($model){
+                    return $model->status->status_type;
+                }
+            ],
             'logo',
             'link',
-            'user_id',
+            [
+                'attribute' => 'user_id',
+                'value' => function($model){
+                    return $model->user->username;
+                },
+            ],
+            [
+                'attribute' => 'date_created',
+                'value' => function($model){
+                    return ($model->date_created) ? date('F d, Y h:i A', strtotime($model->date_created)) : null; 
+                },
+            ],
+            //'user_id',
             'user_update_id',
-            'date_created',
+            //'date_created',
             'date_updated',
         ],
     ]) ?>
