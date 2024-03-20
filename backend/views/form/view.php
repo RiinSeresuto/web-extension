9035a8db21f05c3ce543f58fbf841c26c16085e5
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-03-20 08:33:50 +0800

    added card container

--- backend/views/form/view.php
+++ backend/views/form/view.php
@@ -6,40 +6,104 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Form */
 
-$this->title = $model->id;
+$this->title = $model->category->title;
 $this->params['breadcrumbs'][] = ['label' => 'Forms', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="form-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <!-- <p>
+        <?php // Html::a('<i class="fas fa-edit"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm']) ?>
+        <?php // Html::a('<i class="fas fa-trash-alt"></i> Delete', ['delete', 'id' => $model->id], [
+        //     'class' => 'btn btn-danger btn-sm',
+        //     'data' => [
+        //         'confirm' => 'Are you sure you want to delete this item?',
+        //         'method' => 'post',
+        //     ],
+        // ]) ?>
+    </p> -->
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="card">
+        <div class="card-header">
+            Form Details: <?= $this->title = $model->category->title; ?>
+        </div>
+        <p class="text-right buttons">
+            <?= Html::a('<i class="fas fa-arrow-circle-left"></i> Back', ['index', 'id' => $model->id], ['class' => 'btn btn-info btn-sm']) ?>
+            <?= Html::a('<i class="fas fa-edit"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm']) ?>
+            <?= Html::a('<i class="fas fa-trash-alt"></i> Delete', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger btn-sm',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
+        <div class="card-body">
+            <!-- <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p> -->
+                <?= DetailView::widget([
+                    'model' => $model,
+                    'attributes' => [
+                        [
+                            'attribute' => 'category_id',
+                            'value' => function($model){
+                                return $model->category->title;
+                            },
+                        ],
+                        'description',
+                        [
+                            'attribute' => 'status_id',
+                            'value' => function($model){
+                                return $model->status->status_type;
+                            },
+                        ],
+                        'year',
+                        
+                        [
+                            'attribute' => 'user_id',
+                            'value' => function($model){
+                                return $model->user->username;
+                            },
+                        ],
+                        // [
+                        //     'attribute' => 'user_update_id',
+                        //     'value' => function($model){
+                        //         return $model->user->username;
+                        //     },
+                        // ],
+                        'user_update_id',
+                        [
+                            'attribute' => 'date_created',
+                            'value' => function($model){
+                                return ($model->date_created) ? date('F d, Y h:i A', strtotime($model->date_created)) : null; 
+                            },
+                        ],
+                        [
+                            'attribute' => 'date_updated',
+                            'value' => function($model){
+                                return ($model->date_updated) ? date('F d, Y h:i A', strtotime($model->date_updated)) : null; 
+                            },
+                        ],
+                    ],
+                ]) ?>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            <div class="row view-field">
+                <div class="col-md-12">
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            'field_id',
+                        ]
+                    ])            
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'category_id',
-            'description',
-            'status_id',
-            'year_id',
-            'field_id',
-            'user_id',
-            'user_update_id',
-            'date_created',
-            'date_updated',
-        ],
-    ]) ?>
+    
+
+    
 
 </div>
--- backend/views/menu/view.php
+++ backend/views/menu/view.php
@@ -2,8 +2,6 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
-
-
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
@@ -18,116 +16,130 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="menu-view">
 
-    <p>
-        <?= Html::a('<i class="fas fa-arrow-circle-left"></i> Back', ['index', 'id' => $model->id], ['class' => 'btn btn-secondary btn-sm']) ?>
-        <?= Html::a('<i class="fas fa-info-circle"></i> Detailed View', ['/', 'id' => $model->id], ['class' => 'btn btn-info btn-sm']) ?>
-        <?= Html::a('<i class="fas fa-eye"></i> Actual View', ['/', 'id' => $model->id], ['class' => 'btn btn-success btn-sm']) ?>
-        <?= Html::a('<i class="fas fa-edit"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm']) ?>
-        <?= Html::a('<i class="fas fa-trash-alt"></i> Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger btn-sm',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="card">
+        <div class="card-header">
+            Menu Details: <?= $this->title = $model->label; ?>
+        </div>
+        <p class="text-right buttons">
+            <?= Html::a('<i class="fas fa-arrow-circle-left"></i> Back', ['index', 'id' => $model->id], ['class' => 'btn btn-secondary btn-sm']) ?>
+            <?= Html::a('<i class="fas fa-info-circle"></i> Detailed View', ['/', 'id' => $model->id], ['class' => 'btn btn-info btn-sm']) ?>
+            <?= Html::a('<i class="fas fa-eye"></i> Actual View', ['/', 'id' => $model->id], ['class' => 'btn btn-success btn-sm']) ?>
+            <?= Html::a('<i class="fas fa-edit"></i> Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary btn-sm']) ?>
+            <?= Html::a('<i class="fas fa-trash-alt"></i> Delete', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger btn-sm',
+                'data' => [
+                    'confirm' => 'Are you sure you want to delete this item?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
+        <div class="card-body">
+            <!-- <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a> -->
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            [
-                'attribute' => 'parent_id',
-                'value' => function($model){
-                    if ($model->parentLabel) {
-                        return $model->parentLabel->label;
-                    }
-                }
-            ],
-            'label',
-            'menu_order',
-            [
-                'attribute' => 'position_id',
-                'value' => function($model){
-                    return $model->position->position;
-                },
-            ],
-            [
-                'attribute' => 'status_id',
-                'value' => function($model){
-                    return $model->status->status_type;
-                },
-            ],
-            'link',
-            // [
-            //     'label' => 'Uploaded By',
-            //     'value' => function($model){
-            //         $user_info = yii::$app->user->identity->userinfo;
-            //         return $user_info->FIRST_M.' '.$user_info->LAST_M;
-            //     },
-            // ],
-            [
-                'attribute' => 'user_id',
-                'value' => function($model){
-                    return $model->user->username;
-                },
-            ],
-            [
-                'attribute' => 'date_created',
-                'value' => function($model){
-                    return ($model->date_created) ? date('F d, Y h:i A', strtotime($model->date_created)) : null; 
-                },
-            ],
-            // [
-            //     'attribute' => 'date_updated',
-            //     'value' => function($model){
-            //         return ($model->date_updated) ? date('F d, Y h:i A', strtotime($model->date_updated)) : null; 
-            //     },
-            // ],
-        ],
-    ]) ?>
-    
-
-    <div class="row">
-        <div class="col-md-12">
-            <?=  '<label for="photo_attach">File Attachment</label>' ?>
             <?= DetailView::widget([
                 'model' => $model,
                 'attributes' => [
                     [
-                        'label' => 'File Attachment', 
-                        'format' => 'raw',
+                        'attribute' => 'parent_id',
                         'value' => function($model){
-                        //return \file\components\AttachmentsTable::widget(['model' => $model]); 
-                        return \attachment\components\AttachmentsInput::widget([
-                            'id' => 'file-input', // Optional
-                            'model' => $model,
-                            'options' => [ // Options of the Kartik's FileInput widget
-                                'multiple' => true, // If you want to allow multiple upload, default to false
-                            ],
-                            'pluginOptions' => [ // Plugin options of the Kartik's FileInput widget 
-                                'initialPreviewShowDelete' => false,
-                                'initialPreviewAsData' =>  true,
-                                'initialPreviewFileType' => 'pdf',
-                                'maxFileCount' => 10, // Client max files
-                                'showRemove' => false,
-                                'showCancel' => false,
-                                'showUpload' => false,
-                                'showBrowse' => false,
-                                'showCaption' => false,
-                                'fileActionSettings' => [
-                                    'showRemove' => false,
-                                    //'showDownload' => true,
-                                ],
-                                'previewFileType' => 'pdf'
-                            ],
-                            ]);
+                            if ($model->parentLabel) {
+                                return $model->parentLabel->label;
+                            }
+                        }
+                    ],
+                    'label',
+                    'menu_order',
+                    [
+                        'attribute' => 'position_id',
+                        'value' => function($model){
+                            return $model->position->position;
                         },
-                    ]
-                ]
-            ])
-            ?>
+                    ],
+                    [
+                        'attribute' => 'status_id',
+                        'value' => function($model){
+                            return $model->status->status_type;
+                        },
+                    ],
+                    'link',
+                    // [
+                    //     'label' => 'Uploaded By',
+                    //     'value' => function($model){
+                    //         $user_info = yii::$app->user->identity->userinfo;
+                    //         return $user_info->FIRST_M.' '.$user_info->LAST_M;
+                    //     },
+                    // ],
+                    [
+                        'attribute' => 'user_id',
+                        'value' => function($model){
+                            return $model->user->username;
+                        },
+                    ],
+                    [
+                        'attribute' => 'date_created',
+                        'value' => function($model){
+                            return ($model->date_created) ? date('F d, Y h:i A', strtotime($model->date_created)) : null; 
+                        },
+                    ],
+                    // [
+                    //     'attribute' => 'date_updated',
+                    //     'value' => function($model){
+                    //         return ($model->date_updated) ? date('F d, Y h:i A', strtotime($model->date_updated)) : null; 
+                    //     },
+                    // ],
+                ],
+            ]) ?>
+            
+
+            <div class="row">
+                <div class="col-md-12">
+                    <?=  '<label for="photo_attach">File Attachment</label>' ?>
+                    <?= DetailView::widget([
+                        'model' => $model,
+                        'attributes' => [
+                            [
+                                'label' => 'File Attachment', 
+                                'format' => 'raw',
+                                'value' => function($model){
+                                //return \file\components\AttachmentsTable::widget(['model' => $model]); 
+                                return \attachment\components\AttachmentsInput::widget([
+                                    'id' => 'file-input', // Optional
+                                    'model' => $model,
+                                    'options' => [ // Options of the Kartik's FileInput widget
+                                        'multiple' => true, // If you want to allow multiple upload, default to false
+                                    ],
+                                    'pluginOptions' => [ 
+                                        'initialPreviewShowDelete' => false,
+                                        'initialPreviewAsData' =>  true,
+                                        'initialPreviewFileType' => 'pdf',
+                                        'maxFileCount' => 10, // Client max files
+                                        'showRemove' => false,
+                                        'showCancel' => false,
+                                        'showUpload' => false,
+                                        'showBrowse' => false,
+                                        'showCaption' => false,
+                                        'fileActionSettings' => [
+                                            'showRemove' => false,
+                                            //'showDownload' => true,
+                                        ],
+                                        'previewFileType' => 'pdf'
+                                    ],
+                                    ]);
+                                },
+                            ]
+                        ]
+                    ])
+                    ?>
+                </div>
+            </div>
         </div>
     </div>
+
+    
+
+    
 
     
 </div>
