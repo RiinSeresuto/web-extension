c8e2d9dd47fb46a000d48b865f54984e35a25da3
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-04-11 10:55:06 +0800

    fixed card container

--- backend/views/post/_panel.php
+++ backend/views/post/_panel.php
@@ -3,15 +3,21 @@ use yii\helpers\Url;
 
 ?>
 
-<h3>Forms</h3>
+<div class="card">
+    <div class="card-header">
+        Forms Available
+    </div>
 
-<ul class="nav flex-column">
-    <?php foreach ($forms as $form): ?>
-        <li class="nav-item">
-            <a class="nav-link active" href="<?= Url::toRoute(['post/create', 'form_id' => $form->id]) ?>">
-                <?php echo $form->category->title ?> <br>
-            </a>
-        </li>
+    <div class="card-body">
+        <ul class="nav flex-column">
+            <?php foreach ($forms as $form): ?>
+                <li class="nav-item">
+                    <a class="nav-link active" href="<?= Url::toRoute(['post/create', 'form_id' => $form->id]) ?>">
+                        <?php echo $form->category->title ?> <br>
+                    </a>
+                </li>
 
-    <?php endforeach; ?>
-</ul>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
