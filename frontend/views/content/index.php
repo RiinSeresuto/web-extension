4b19608df81ea247bedd150cfcfdae478efad289
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-04-12 08:40:37 +0800

    fixed card container

--- frontend/views/content/index.php
+++ frontend/views/content/index.php
@@ -1,13 +1,17 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<div class="card">
-    <div class="card-header">Content</div>
-    <div class="card-body">
-    <?php 
-        echo  $page->body;
-    ?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-header">Content</div>
+        <div class="card-body">
+            <?php 
+            echo  $page->body;
+            ?>
+        </div>
     </div>
 </div>
+
 
 
