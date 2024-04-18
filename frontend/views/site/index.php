50ebe46c847625f822a96476353cda451734d3d5
Sheila Marie Senin <ellaoaeia@gmail.com>
2024-04-18 09:36:29 +0800

    fixed displaying of centrapl news data

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,3 +5,11 @@
 $this->title = 'DILG: Department of the Interior and Local Government';
 
 ?>
+
+<div class="container">
+    <?= $this->render('_central_news_highlight', [
+        'central_news_highlight' => $central_news[0]->body,
+        'central_news_date' => $central_news[0]->date_created,
+        'central_news' => $central_news,
+    ]); ?>
+</div>
