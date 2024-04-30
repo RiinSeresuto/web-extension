a7f3595e5a7fc571ca45c3b1923659310e84d499
stsenin <stsenin@dilg.gov.ph>
2024-04-30 15:22:05 +0800

    added function for displaying of image of dilg info system carousel

--- frontend/controllers/CarouselController.php
+++ frontend/controllers/CarouselController.php
@@ -56,5 +56,30 @@ class CarouselController extends Controller
             return 'Image not found';
         }
     }
+
+    public static function actionImageDilgSystem($item_id)
+    {
+        $modelImg = File::find()->where(['item_id' => $item_id])->andWhere(['model' => "backend\models\DilgInfoSystems"])->one();
+
+        $folder1 = substr(basename($modelImg->hash), 0, 2) . "/";
+        $folder2 = substr(basename($modelImg->hash), 3, 2) . "/";
+        $folder3 = substr(basename($modelImg->hash), 6, 2) . "/";
+        $filename = $modelImg->hash . "." . $modelImg->type;
+
+        $link = '@common/uploads/store/' . $folder1 . $folder2 . $folder3 . $filename;
+        $path = Yii::getAlias($link);
+        // $files = FileHelper::findFiles($path);
+
+        if (file_exists($path)) {
+            // return $path;
+            $imageData = file_get_contents($path);
+            $contentType = mime_content_type($path);
+            Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
+            Yii::$app->response->headers->add('Content-Type', $contentType);
+            return $imageData;
+        } else {
+            return 'Image not found';
+        }
+    }
 }
 ?>
