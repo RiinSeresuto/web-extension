4fe8b06059d235c131a10c5f7f93e83f4c6c1797
stsenin <stsenin@dilg.gov.ph>
2024-04-30 15:43:45 +0800

    expanded carousel helper with getInfoSystem method

--- common/helpers/Carousel.php
+++ common/helpers/Carousel.php
@@ -2,6 +2,7 @@
 
 namespace common\helpers;
 
+use backend\models\DilgInfoSystems;
 use Yii;
 use backend\models\SliderPhoto;
 use backend\models\Banner;
@@ -21,7 +22,12 @@ class Carousel
 
     public static function getBanner()
     {
-        return Banner::find()->all();
+        return Banner::find()->where(['status_id' => 1])->all();
+    }
+
+    public static function getInfoSystem()
+    {
+        return DilgInfoSystems::find()->where(['status_id' => 1])->all();
     }
 }
 ?>
